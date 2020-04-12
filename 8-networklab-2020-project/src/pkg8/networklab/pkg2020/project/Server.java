1eb899cb5ce1a596eadf0e2930e9b9679622b460
Abdulhalik <abdlhlkkrkmz@gmail.com>
2020-04-12 20:29:12 +0300

    ClientUI and supportive filed added

---
+++ 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Server.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg8.networklab.pkg2020.project;
+
+/**
+ *
+ * @author abdulhalik
+ */
+public class Server {
+    
+}
