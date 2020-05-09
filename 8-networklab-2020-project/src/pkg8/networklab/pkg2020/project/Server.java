42ac729742c9213b0cadaa478653f0e22de44377
halil ibrahim közen <37106104+HalilibrahimKOZEN@users.noreply.github.com>
2020-05-09 22:00:58 +0300

    Ready for Server and Socket connection -- Halil

--- 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Server.java
+++ 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Server.java
@@ -3,15 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg8.networklab.pkg2020.project;
+package socketfiletransferdemo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author abdulhalik
  * @author halil
- * 
+ *
  * Ekleme yaptığımız her kod boluğu için yorum satırı eklemeye çalışalım, anlaşılır olsun.
  */
-
 public class Server {
-    
+
+    private ServerSocket serverSocket = null;
+    private Socket socket = null;
+    private ObjectInputStream inputStream = null;
+
+    public Server() {
+
+    }
+
+    /**
+     * Socket bağlantılarının kabul edilmesi.
+     */
+
+
+    public void doConnect() {
+        try {
+            serverSocket = new ServerSocket(4445);
+            socket = serverSocket.accept();
+            System.out.println(socket.getPort());
+            inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.doConnect();
+    }
+
 }
