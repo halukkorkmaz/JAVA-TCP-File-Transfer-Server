74bdecb94c8e65fa9f4960adf267b722cc347e3b
Abdulhalık Korkmaz <abdlhlkkrkmz@gmail.com>
2020-05-09 22:07:44 +0300

    Added File Object & Download Func -- Abdulhalik

--- 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Server.java
+++ 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Server.java
@@ -23,6 +23,9 @@ public class Server {
     private ServerSocket serverSocket = null;
     private Socket socket = null;
     private ObjectInputStream inputStream = null;
+    private FileEvent fileEvent;
+    private File dstFile = null;
+    private FileOutputStream fileOutputStream = null;
 
     public Server() {
 
@@ -44,10 +47,43 @@ public class Server {
         }
     }
 
+    /**
+     * FileEvent nesnesini okuma ve dosyayı diske kopyalama.
+     */
+    public void downloadFile() {
+        try {
+            fileEvent = (FileEvent) inputStream.readObject();
+            if (fileEvent.getStatus().equalsIgnoreCase("Error")) {
+                System.out.println("Error occurred..");
+                System.exit(0);
+            }
+            String outputFile = fileEvent.getDestinationDirectory() + fileEvent.getFilename();
+            if (!new File(fileEvent.getDestinationDirectory()).exists()) {
+                new File(fileEvent.getDestinationDirectory()).mkdirs();
+            }
+            dstFile = new File(outputFile);
+            fileOutputStream = new FileOutputStream(dstFile);
+            fileOutputStream.write(fileEvent.getFileData());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            System.out.println("Output file : " + outputFile + " başarı ile kaydedildi. ");
+            Thread.sleep(3000);
+            System.exit(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         Server server = new Server();
         server.doConnect();
+        server.downloadFile();
     }
 
 }
