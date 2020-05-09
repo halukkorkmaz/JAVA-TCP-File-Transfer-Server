ec3dc8a8348afc3ecb441de9db953312c55c83c7
fatihmehmetergin <38405979+fatihmehmetergin@users.noreply.github.com>
2020-05-10 00:03:29 +0300

    Added SendFile method -- Fatih

--- 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Client.java
+++ 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Client.java
@@ -18,6 +18,12 @@ public class Client {
 
     private Socket socket = null;
     private ObjectOutputStream outputStream = null;
+    private boolean isConnected = false;
+    private FileEvent fileEvent = null;
+    // ----- TEST -----
+    // Path'i kendi dosya sisteminize göre değiştrirerek test edin!!!!!
+    private String sourceFilePath = "/Users/TEST/Desktop/SouTesting/dosya.pdf";
+    private String destinationPath = "/Users/TEST/Desktop/DesTesting/";
 
     public Client() {
 
@@ -38,8 +44,55 @@ public class Client {
         }
     }
 
+    /**
+     * Dosya objesini yolla.
+     */
+    public void sendFile() {
+        fileEvent = new FileEvent();
+        String fileName = sourceFilePath.substring(sourceFilePath.lastIndexOf("/") + 1, sourceFilePath.length());
+        String path = sourceFilePath.substring(0, sourceFilePath.lastIndexOf("/") + 1);
+        fileEvent.setDestinationDirectory(destinationPath);
+        fileEvent.setFilename(fileName);
+        fileEvent.setSourceDirectory(sourceFilePath);
+        File file = new File(sourceFilePath);
+        if (file.isFile()) {
+            try {
+                DataInputStream diStream = new DataInputStream(new FileInputStream(file));
+                long len = (int) file.length();
+                byte[] fileBytes = new byte[(int) len];
+                int read = 0;
+                int numRead = 0;
+                while (read < fileBytes.length && (numRead = diStream.read(fileBytes, read, fileBytes.length - read)) >= 0) {
+                    read = read + numRead;
+                }
+                fileEvent.setFileSize(len);
+                fileEvent.setFileData(fileBytes);
+                fileEvent.setStatus("Success");
+            } catch (Exception e) {
+                e.printStackTrace();
+                fileEvent.setStatus("Error");
+            }
+        } else {
+            System.out.println("Belirtilen konumda dosya bulunamadi.");
+            fileEvent.setStatus("Error");
+        }
+//Dosya objesini sokete yazma.
+        try {
+            outputStream.writeObject(fileEvent);
+            System.out.println("Oturum sonlandırıldı..");
+            Thread.sleep(3000);
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         Client client = new Client();
         client.connect();
+        client.sendFile();
     }
 }
