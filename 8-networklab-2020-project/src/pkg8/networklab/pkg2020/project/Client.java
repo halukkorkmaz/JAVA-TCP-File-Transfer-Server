b92ce6fa70dac818cdf5bb686987fa33e6c0eff4
sametemretopaloglu <43732189+sametemretopaloglu@users.noreply.github.com>
2020-05-09 22:43:23 +0300

    Added Client Socket Connection -- Samet

--- 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Client.java
+++ 8-networklab-2020-project/src/pkg8/networklab/pkg2020/project/Client.java
@@ -5,13 +5,41 @@
  */
 package pkg8.networklab.pkg2020.project;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * @author fatih
  * @author samet
- * 
+ *
  * Ekleme yaptığımız her kod boluğu için yorum satırı eklemeye çalışalım, anlaşılır olsun.
  */
-
 public class Client {
-    
+
+    private Socket socket = null;
+    private ObjectOutputStream outputStream = null;
+
+    public Client() {
+
+    }
+
+    /**
+     * Localhost veya diğer hostlar ile server'a bağlan.
+     */
+    public void connect() {
+        while (!isConnected) {
+            try {
+                socket = new Socket("localHost", 4445);
+                outputStream = new ObjectOutputStream(socket.getOutputStream());
+                isConnected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+    }
 }
